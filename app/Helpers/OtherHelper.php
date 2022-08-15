2aad459ef369829f0df75e87803d006edd8a2940
Omobowale <omobowale92@outlook.com>
2022-08-15 13:16:29 +0100

    Minor changes

--- app/Helpers/OtherHelper.php
+++ app/Helpers/OtherHelper.php
@@ -6,8 +6,8 @@ function loggedInUserIsAdmin(){
     $res2 = Session::has('is_admin');
     $res = $res1 && $res2;
     return $res;
-    
 }
+
 function loggedInUserAsRegularUser(){
     $res = Session::has('is_regular_user');
     return $res;
--- resources/views/components/user-message-table-header.blade.php
+++ resources/views/components/user-message-table-header.blade.php
@@ -1,11 +1,11 @@
 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
     <tr>
-        @if (auth()->user()->isAdmin())
-            <th scope="col" class="py-3 px-6">
-
-                User's name
-            </th>
-        @endif
+       
+        @if (loggedInUserIsAdmin())
+        <td scope="col" class="py-3 px-6">
+            User name
+        </td>
+    @endif
         <th scope="col" class="py-3 px-6">
             {{ $col1 }}
         </th>
